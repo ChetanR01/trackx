50847dfce0c7707a3159c196e1c733b8e7eb1ff2
Chetan Rathod <chetan0101career@gmail.com>
2024-12-27 21:51:19 +0530

    completed the trackx project

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:trackx/widgets/expenses_list/expenses_list.dart';
 import 'package:trackx/models/expense.dart';
+import 'package:trackx/widgets/new_expense.dart';
+import 'package:trackx/widgets/chart/chat.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -27,16 +29,69 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registerdExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registerdExpenses.indexOf(expense);
+    setState(() {
+      _registerdExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense Deleted!'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registerdExpenses.insert(expenseIndex, expense);
+              });
+            }),
+      ),
+    );
+  }
+
   @override
   Widget build(context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registerdExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registerdExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('TrackX'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
-          const Text('Chart'),
+          Chart(expenses: _registerdExpenses),
           Expanded(
-            child: ExpensesList(
-              expenses: _registerdExpenses,
-            ),
+            child: mainContent,
           )
         ],
       ),
